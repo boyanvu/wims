f9cc163cfa1844930782e7af1e0b8a75f68dffa5
Boyan Vuchev <bvuchev@gmail.com>
2020-02-15 15:24:18 +0200

    Implemented CurrentVariables class

---
+++ Skeleton/Wims/Core/CurrentVariables.cs
@@ -0,0 +1,10 @@
+﻿using Wims.Models.Contracts;
+
+namespace Wims.Core
+{
+    public static class CurrentVariables
+    {
+        public static ITeam currentTeam;
+        public static IBoard currentBoard;
+    }
+}
