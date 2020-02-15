6b5a38644b953937f9acedf9d76ad602341be1e1
Boyan Vuchev <bvuchev@gmail.com>
2020-02-15 19:12:13 +0200

    CreateBoardCommand implemented

--- Skeleton/Wims/Core/Commands/CreateBoardCommand.cs
+++ Skeleton/Wims/Core/Commands/CreateBoardCommand.cs
@@ -13,6 +13,7 @@ namespace Wims.Core.Commands
         {
             this.BoardProvider = boardProvider;
         }
+
         public IBoardProvider BoardProvider { get; }
 
         public override string Execute()
@@ -22,11 +23,33 @@ namespace Wims.Core.Commands
                 throw new ArgumentException("Parameters count is not valid!");
             }
 
+            var currTeam = CurrentVariables.currentTeam;
+
+            if (currTeam == null)
+            {
+                var msg = $"You have to select/create team before board creation." + Environment.NewLine +
+                          $"You can you use one of these commands:" + Environment.NewLine +
+                          $"createteam <teamname>, selectteam <teamname>, listteams";
+                throw new Exception(msg);
+            }
+
+            var boardName = this.CommandParameters[0];
+            foreach (var boardInTeam in currTeam.Boards)
+            {
+                if (boardInTeam.Name == boardName)
+                {
+                    var msg = $"Board with name {boardName} already exists in team {currTeam.Name}" + Environment.NewLine +
+                              $"You can see all available boards with command listboards.";
+                    throw new ArgumentException(msg);
+                }
+            }
+
             var board = this.Factory.CreateBoard(
-                this.CommandParameters[0]
+                boardName
             );
 
             this.BoardProvider.Add(board);
+            currTeam.Boards.Add(board);
 
             return $"Created Board {Environment.NewLine}{board.Print()}";
         }
