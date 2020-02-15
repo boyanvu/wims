859111bbbf7b77792ed1ec6e94bf18ff53580d86
Boyan Vuchev <bvuchev@gmail.com>
2020-02-15 21:26:57 +0200

    ListAllTeamBoardsCommand implemented

---
+++ Skeleton/Wims/Core/Commands/ListAllTeamBoardsCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wims.Core.Commands.Abstracts;
+using Wims.Core.Contracts;
+
+namespace Wims.Core.Commands
+{
+    public class ListAllTeamBoardsCommand : Command
+    {
+        public ListAllTeamBoardsCommand(IList<string> commandLine, IBoardProvider boardProvider) :
+            base(commandLine)
+        {
+            this.BoardProvider = boardProvider;
+        }
+
+        public IBoardProvider BoardProvider { get; }
+
+
+        public override string Execute()
+        {
+
+            var builder = new StringBuilder();
+            var boards = CurrentVariables.currentTeam.Members;
+
+            foreach (var board in boards)
+            {
+                builder.AppendLine(board.Print());
+            }
+
+            return builder.ToString().TrimEnd();
+        }
+    }
+}
