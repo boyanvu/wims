5b5becad51cd4dfd38f531d7fa875fc11a4731b0
Boyan Vuchev <bvuchev@gmail.com>
2020-02-23 23:52:58 +0200

    Tests for Member creation implemented

---
+++ Skeleton/Wims.Tests/Models.Tests/Member.Tests/Constructor_Should.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Wims.Models;
+
+namespace Wims.Tests.Models
+
+{
+    [TestClass]
+    public class TestMember
+    {
+        [TestMethod]
+        public void MemberConstructorCreateValid()
+        {
+            // Arrange & Act
+            Member testMember = new Member("Tosho");
+
+            //Assert
+            Assert.IsInstanceOfType(testMember, typeof(Member));
+        }
+
+        [TestMethod]
+        public void MemberShouldSetCorrectName()
+        {
+            // Arrange & Act
+            var testMember = new Member("Tosho");
+
+            //Assert
+            Assert.AreEqual("Tosho", testMember.Name);
+        }
+
+        [TestMethod]
+        public void MemberShouldThrowEx()
+        {
+            //Arange, Act & Assert
+            Assert.ThrowsException<ArgumentException>(
+                () => new Member("Pepi"), "Member name must be between 5 and 15 characters long!");
+        }
+
+
+        [TestMethod]
+        public void MemberShouldThrowExAndMessageWithNull()
+        {
+            //Arange, Act & Assert
+            Assert.ThrowsException<ArgumentNullException>(
+                 () => new Member(null), "Member name must not be null!");
+        }
+    }
+}
+
