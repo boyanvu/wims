0292d560adc31cb71a2024b32d0b3d0936edac7b
Radoslav Simeonov <rsimeonovv@abv.bg>
2020-02-25 13:06:48 +0200

    Unassign test implemented.

---
+++ Skeleton/Wims.Tests/Commands.Test/Assigning.Test/Unassigning_should.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Wims.Core.Contracts;
+using Wims.Models;
+using Wims.Models.Contracts;
+using Wims.Models.Common;
+using Wims.Core.Commands;
+using Wims.Core;
+
+namespace Wims.Tests.Commands.Test.Assigning.Test
+{
+    [TestClass]
+    public class Unassign_Should
+    {
+        [TestMethod]
+        public void ThrowWhen_NoWorkItemsInBoard()
+        {
+            //Arrange
+            var list = new List<string>();
+            var sut = new UnassignCommand(list);
+
+            //Act & Assert
+            Assert.ThrowsException<ArgumentException>(() => sut.Execute());
+        }
+
+        [TestMethod]
+        public void ReturnCorrectString()
+        {
+            //Arrange
+            //var team = new Team("Ducks");
+            var board = new Board("Trello");
+            //CurrentVariables.currentTeam = team;
+            CurrentVariables.currentBoard = board;
+            var currBoardItems = CurrentVariables.currentBoard;
+            var story = new Story("NewCreatedStory", "NewCreateBugStoryDescr", Priority.Low, Size.Large);
+            var member = new Member("Pesho");
+            story.Assignee = member;
+            currBoardItems.WorkItems.Add(story);
+            var listParams = new List<string>() { story.Title };
+            var sut = new UnassignCommand(listParams);
+
+            //Act
+            var result = sut.Execute();
+
+            //Assert
+            Assert.AreEqual(result, "Pesho has been unassigned from NewCreatedStory");
+        }
+
+    }
+}
