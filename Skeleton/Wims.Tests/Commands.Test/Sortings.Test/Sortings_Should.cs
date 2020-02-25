0e0b3b0ed7dfa38687232d123ff40f0748880f4a
Boyan Vuchev <bvuchev@gmail.com>
2020-02-25 10:53:39 +0200

    BugsBySeverity test implemented

---
+++ Skeleton/Wims.Tests/Commands.Test/Sortings.Test/Sortings_Should.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Wims.Core.Commands;
+using Wims.Core.Contracts;
+using Wims.Models;
+using Wims.Models.Contracts;
+using Wims.Models.Common;
+
+namespace Wims.Tests.Commands.Tests.Sorting.Tests.SortBugsBySeverityCommandTests
+{
+    [TestClass]
+    public class Execute_Should
+    {
+        [TestMethod]
+        public void ThrowWhen_NoItemsInBoard()
+        {
+            //Arrange
+            var fakeProvider = new FakeWorkItemProvider();
+            var list = new List<string>();
+            var sut = new SortBugsBySeverityCommand(list, fakeProvider);
+
+            //Act & Assert
+            Assert.ThrowsException<ArgumentException>(() => sut.Execute());
+        }
+
+        [TestMethod]
+        public void ReturnCorrectString()
+        {
+            //Arrange
+            var fakeProvider = new FakeWorkItemProvider();
+
+            var listParams = new List<string>();
+            //string title, string description, Priority priority, Severity severity
+            var bug1 = new Bug("WindowsCrash", "Description", Priority.High, Severity.Critical);
+            var bug2 = new Bug("CrashCrash", "NovDescription", Priority.Low, Severity.Minor);
+            fakeProvider.Add(bug1);
+            fakeProvider.Add(bug2);
+            var sut = new SortBugsBySeverityCommand(listParams, fakeProvider);
+
+            //Act
+            var result = sut.Execute();
+
+            //Assert
+            Assert.IsTrue(result.StartsWith($"Bug:{Environment.NewLine}  Title: WindowsCrash"));
+        }
+    }
+    class FakeWorkItemProvider : IWorkItemProvider
+    {
+        private readonly List<IWorkItem> workItems = new List<IWorkItem>();
+        public IReadOnlyList<IWorkItem> WorkItems => workItems;
+
+        public void Add(IWorkItem item)
+        {
+            workItems.Add(item);
+        }
+
+        public IWorkItem Find(string title)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
