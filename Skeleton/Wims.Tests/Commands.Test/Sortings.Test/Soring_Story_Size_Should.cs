e7dda0efdfbc82efd02f540ce593986c9bf5a49d
Boyan Vuchev <bvuchev@gmail.com>
2020-02-27 08:58:32 +0200

    Sorting_Story_Size_Should implemented

---
+++ Skeleton/Wims.Tests/Commands.Test/Sortings.Test/Soring_Story_Size_Should.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Wims.Core.Commands.Sorting;
+using Wims.Models.Common;
+
+namespace Wims.Tests.Commands.Test.Sortings.Test
+{
+    [TestClass]
+    public class Sorting_Story_Size_Should
+    {
+        [TestMethod]
+        public void ReturnCorrectString()
+        {
+            //Arrange
+            var fakeProvider = new FakeWorkItemProvider();
+
+            var storyA = new Story("TitleStoryA", "DescriptionStoryA", Priority.High, Size.Small);
+            var storyB = new Story("TitleStoryB", "DescriptionStoryB", Priority.High, Size.Large);
+
+            fakeProvider.Add(storyA);
+            fakeProvider.Add(storyB);
+
+            var list = new List<string>();
+            var sut = new SortStoriesBySizeCommand(list, fakeProvider);
+
+            //Act
+            var result = sut.Execute();
+
+            //Assert
+            Assert.IsTrue(result.StartsWith($"Story:{Environment.NewLine}  Title: TitleStoryB"));
+
+
+        }
+
+        [TestMethod]
+        public void NoStoriesToBeSorted_ThrowEx()
+        {
+            //Arrange
+            var fakeProvider = new FakeWorkItemProvider();
+
+            var list = new List<string>();
+            var sut = new SortStoriesBySizeCommand(list, fakeProvider);
+
+            //Act & Assert
+            Assert.ThrowsException<ArgumentException>(() => sut.Execute(), "No items in this board!");
+
+
+        }
+    }
+}
