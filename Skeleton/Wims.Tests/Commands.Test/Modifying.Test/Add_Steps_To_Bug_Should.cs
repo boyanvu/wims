9e2de3da9242b30bd3bb23c6bf4f3694f28c4edb
Radoslav Simeonov <rsimeonovv@abv.bg>
2020-02-27 12:33:19 +0200

    Add_Steps_To_Bug_Should test implemented

---
+++ Skeleton/Wims.Tests/Commands.Test/Modifying.Test/Add_Steps_To_Bug_Should.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wims.Core;
+using Wims.Core.Commands;
+using Wims.Core.Commands.Modifying;
+using Wims.Core.Contracts;
+using Wims.Models;
+using Wims.Models.Common;
+using Wims.Models.Contracts;
+
+namespace Wims.Tests.Commands.Test.Modifying.Test
+{
+    [TestClass]
+    public class Steps_To_Bug_Should
+    {
+
+        [TestMethod]
+        public void ThrowWhen_NoItemsInBoard()
+        {
+            //Arrange
+            var list = new List<string>();
+            var sut = new AddStepsToBugCommand(list);
+
+            //Act & Assert
+            Assert.ThrowsException<ArgumentException>(() => sut.Execute());
+        }
+
+        [TestMethod]
+        public void Execute_Less_Params_ThrowEx()
+        {
+            var listParams = new List<string>() { };
+            var sut = new AddStepsToBugCommand(listParams);
+            //Act & Assert
+            Assert.ThrowsException<ArgumentException>(() => sut.Execute(), "Parameters count is not valid!");
+        }
+
+        [TestMethod]
+        public void ReturnCorrectString()
+        {
+            //Arrange
+            var board = new Board("Trello");
+            var bug = new Bug("NewCreatedBug", "NewCreateBugDescription", Priority.High, Severity.Minor);
+            var listParams = new List<string>() { "NewCreatedBug", "first step>", "second step>", "third step" };
+            CurrentVariables.currentBoard = board;
+            var currBoardItems = CurrentVariables.currentBoard.WorkItems;
+            currBoardItems.Add(bug);
+            var sut = new AddStepsToBugCommand(listParams);
+
+            //Act
+            var result = sut.Execute();
+
+            //Assert
+            Assert.AreEqual(result, $"Steps to reproduce have been added to bug NewCreatedBug.");
+        }
+
+
+    }
+}
